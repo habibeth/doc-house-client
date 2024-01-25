f6cf9f86cbf0d3609a4e19b2ca6d1db2ebe15d88
Ahsan Habib <habibstk97@gmail.com>
2024-01-25 07:48:31 +0600

    book now modal

--- src/Pages/Appointment/AppointmentOption/AppointmentOption.jsx
+++ src/Pages/Appointment/AppointmentOption/AppointmentOption.jsx
@@ -1,9 +1,21 @@
 
 
-const AppointmentOption = () => {
+const AppointmentOption = ({ appointment }) => {
+    console.log(appointment)
+    const { name, image } = appointment;
     return (
-        <div>
-            
+        <div className="">
+            <div className="shadow-xl rounded-xl">
+                <div className="rounded-lg flex justify-center my-3 h-32">
+                    <img src={image} alt="" className="bg-pink-200 p-7 rounded-full" />
+                </div>
+                <div className="">
+                    <h2 className="text-2xl font-bold mt-7 text-center">{name}</h2>
+                </div>
+                <div className="flex justify-center mt-10 pb-14">
+                    <button className="btn bg-[#F7A582] text-white">Book Appointment</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Appointment/AppointmentOption/AvailableAppointment.jsx
+++ src/Pages/Appointment/AppointmentOption/AvailableAppointment.jsx
@@ -1,20 +1,31 @@
 import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
 import 'react-day-picker/dist/style.css';
+import useAxiosPublic from '../../../hooks/useAxiosPublic'
+import AppointmentOption from './AppointmentOption'
+import { useState } from 'react';
 
 const AvailableAppointment = ({selected}) => {
+    const [treatment, setTreatment] = useState(null)
     const date = format(selected, 'PP');
+    const axiosPublic = useAxiosPublic() 
     const {data: appointmentOption, refetch, isLoading} = useQuery({
         queryKey: ['appointmentOption', date],
         queryFn: async()=>{
-            const res = await 
+            const res = await axiosPublic.get(`/appointmentOption?date=${date}`)
+            return res.data;
         }
     })
+    // console.log(appointmentOption)
     return (
-        <div className='mt-20'>
+        <div className='mt-20 max-w-6xl mx-auto'>
             <p className='text-2xl text-[#F7A582] text-center'>Available Services on {date}</p>
-            <h2 className='text-4xl font-bold text-center mt-3'>Please select a service.</h2>
-
+            <h2 className='text-4xl font-bold text-center my-3'>Please select a Available slots from you Need.</h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-5 items-stretch mt-12 mb-32">
+                {
+                    appointmentOption?.map(appointment=> <AppointmentOption key={appointment._id} appointment={appointment}></AppointmentOption>)
+                }
+            </div>
         </div>
     );
 };
